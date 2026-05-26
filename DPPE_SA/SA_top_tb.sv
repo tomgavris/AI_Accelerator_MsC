9dab31edb68308b88c012a23682e6e225704efee
tgavrisa <tgavrisa@auth.gr>
2026-05-26 11:20:51 +0200

    Addition of testbench for SA_top

---
+++ DPPE_SA/SA_top_tb.sv
@@ -0,0 +1,86 @@
+import pe_pkg::*;
+
+module SA_top_tb ();
+    
+logic signed [M-1:0][N-1:0][OP-1:0][DATA_WIDTH-1:0]                top_a_i;
+logic signed [M-1:0][N-1:0][2*DATA_WIDTH-1:0]                      top_p_i;
+logic                                                              top_clk, top_rst, top_wr_e, top_comp_e;
+logic signed [M-1:0][M-1:0][N-1:0][N-1:0][OP-1:0][DATA_WIDTH-1:0]  top_w_i;
+logic signed [M-1:0][N-1:0][OP-1:0][DATA_WIDTH-1:0]                top_a_o;
+logic signed [M-1:0][N-1:0][2*DATA_WIDTH-1:0]                      top_parsum_o; 
+
+
+SA_top SA_top_i (
+    .top_a_i(top_a_i),
+    .top_p_i(top_p_i),
+    .top_clk(top_clk), 
+    .top_rst(top_rst), 
+    .top_wr_e(top_wr_e), 
+    .top_comp_e(top_comp_e),
+    .top_w_i(top_w_i),
+    .top_a_o(top_a_o),
+    .top_parsum_o(top_parsum_o)
+);
+initial begin
+    top_clk = 0;
+    forever #5 top_clk = ~top_clk;  
+end
+
+initial begin
+    for (int i = 0; i < M; i++) begin
+        for (int j = 0; j < N ; j++ ) begin
+            for (int k = 0; k < OP; k++) begin
+                top_a_i[i][j][k] =  i + j + k;
+            end
+        end
+    end
+    
+
+    for (int i = 0; i < M ; i++ ) begin
+        for (int j = 0; j < N; j++) begin
+            top_p_i[i][j] = 1;
+        end
+    end
+    
+    for (int i = 0; i < M; i++) begin
+        for (int j = 0; j < M; j++) begin
+            for (int k = 0; k < N ; k++ ) begin
+                for (int l = 0; l < N ; l++ ) begin
+                    for (int m = 0; m < OP; m++) begin
+                        top_w_i[i][j][k][l][m] = i + j + k + l + m;
+                    end
+                end
+            end
+        end
+    end
+    top_rst = 1;
+    top_wr_e = 0;
+    top_comp_e = 0;
+
+    @(posedge top_clk);
+    top_rst = 0;
+    top_wr_e = 1;
+    top_comp_e = 0;
+
+    @(posedge top_clk);
+    top_rst = 0;
+    top_wr_e = 0;
+    top_comp_e = 1;
+
+    repeat(9) @(posedge top_clk);
+    top_rst = 1;
+    top_wr_e = 0;
+    top_comp_e = 0;
+
+    @(posedge top_clk);
+    top_rst = 0;
+    top_wr_e = 0;
+    top_comp_e = 0;
+
+    repeat(4) @(posedge top_clk);
+    $finish;
+
+
+end
+
+endmodule
