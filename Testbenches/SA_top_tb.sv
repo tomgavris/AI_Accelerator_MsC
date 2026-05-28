f848bfa1e09cd5014f7ab9cfd20fd28c258c4fec
tgavrisa <tgavrisa@auth.gr>
2026-05-28 14:50:36 +0200

    Changed file structure

--- Testbenches/SA_top_tb.sv
+++ Testbenches/SA_top_tb.sv
@@ -1,5 +1,8 @@
 import pe_pkg::*;
 
+/* this testbench is meant to handle activations with a 
+time domain skew, simulating real life activation streaming
+*/
 module SA_top_tb ();
     
 logic signed [M-1:0][N-1:0][OP-1:0][DATA_WIDTH-1:0]                top_a_i;
@@ -30,13 +33,7 @@ end
 initial begin
     
     static int weight_val = 1;
-    for (int i = 0; i < M; i++) begin
-        for (int j = 0; j < N ; j++ ) begin
-            for (int k = 0; k < OP; k++) begin
-                top_a_i[i][j][k] =  i + j + k;
-            end
-        end
-    end
+    
     
 
     for (int i = 0; i < M ; i++ ) begin
@@ -54,23 +51,47 @@ initial begin
         for (int j = 0; j < N; j++) begin
             for (int k = 0; k < N; k++) begin
                 for (int l = 0; l < OP ; l++) begin
-                    top_w_i[i][j][k][l] = weight_val;
+                    top_w_i[i][j][k][l] = weight_val; // TODO: make the weights roll
+                    weight_val = weight_val + 1;
                 end
             end
         end
     end
-    weight_val = weight_val + 1;
+    
     top_rst = 0;
     top_wr_e = 1;
     top_comp_e = 0;
 
 
-    repeat(9) @(posedge top_clk);
+    repeat(2) @(posedge top_clk);
+    
+    for (int i = 0; i < M ; i++) begin
+        for (int j = 0; j < N; j++) begin
+            for (int k = 0; k < N; k++) begin
+                for (int l = 0; l < OP ; l++) begin
+                    top_w_i[i][j][k][l] = weight_val; // TODO: make the weights roll
+                    weight_val = weight_val + 1;
+                end
+            end
+        end
+    end
+
+    @(posedge top_clk);
+
     top_rst = 0;
     top_wr_e = 0;
     top_comp_e = 1;
 
-    repeat(9) @(posedge top_clk);
+    for (int i = 0; i < M; i++) begin
+        for (int j = 0; j < N ; j++ ) begin
+            for (int k = 0; k < OP; k++) begin
+                top_a_i[i][j][k] =  i + j + k;
+            end
+        end
+        @(posedge top_clk);
+    end
+
+    repeat(8) @(posedge top_clk);
     top_rst = 1;
     top_wr_e = 0;
     top_comp_e = 0;
