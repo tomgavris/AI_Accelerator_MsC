47bc33542a6c64f429241af6aa988025ed41f3a6
tgavrisa <tgavrisa@auth.gr>
2026-06-11 17:01:31 +0200

    Changed file location

---
+++ pe_pkg.sv
@@ -0,0 +1,9 @@
+package pe_pkg;
+    localparam DATA_WIDTH = 8;
+    localparam P_DATA_WIDTH = 16;
+    localparam OP = 2;
+    localparam N = 2;
+    localparam M = 2;
+    localparam CYCLES = 2;
+    localparam W_CYCLES = M;
+endpackage
