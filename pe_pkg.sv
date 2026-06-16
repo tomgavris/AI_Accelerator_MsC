a93d7bfa04213fc6616967bea7fe6f8542ac1c28
tgavrisa <tgavrisa@auth.gr>
2026-06-16 13:59:21 +0200

    Added memory related parameters

--- pe_pkg.sv
+++ pe_pkg.sv
@@ -1,9 +1,22 @@
 package pe_pkg;
+
+    // parameters relating to data width
     localparam DATA_WIDTH = 8;
     localparam P_DATA_WIDTH = 16;
+
+    // parameters relating to the SA architecture
     localparam OP = 2;
     localparam N = 2;
     localparam M = 2;
+
+
     localparam CYCLES = 2;
     localparam W_CYCLES = M;
+
+    // parameters relating to memory
+    localparam PARTITIONS = 4;
+    localparam SP_SIZE = 2048; // Scratchpad size
+    localparam SRAM_SIZE = SP_SIZE/PARTITIONS;
+    localparam CONC_ADD = M/PARTITIONS; // Concurrent addresses read from each partition
+    localparam SRAM_WIDTH = N*OP*DATA_WIDTH;
 endpackage
