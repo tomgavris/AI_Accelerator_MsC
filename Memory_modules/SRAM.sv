afa6a323fdcd39d0b362095a376b5e374d12c2f2
tgavrisa <tgavrisa@auth.gr>
2026-06-22 14:13:02 +0200

    Changed logic from FIFO to Single Port RAM

--- Memory_modules/SRAM.sv
+++ Memory_modules/SRAM.sv
@@ -1,38 +1,26 @@
 import pe_pkg::*;
 
-module SRAM (
-    input  logic signed [SRAM_WIDTH-1:0] sram_i,
-    input  logic                         clk, rst, sram_wr, sram_rd,
-    output logic signed [SRAM_WIDTH-1:0] sram_o
-);
-    
-    logic [SRAM_WIDTH-1:0] mem [0:SRAM_SIZE-1];
-    logic [$clog2(SRAM_SIZE)-1:0]  wr_pointer ,rd_pointer;
+module single_port_ram
+  ( 	input  logic					            clk, rst,
+   		input  logic        [$clog2(SRAM_SIZE)-1:0]	addr,
+   		input  logic signed [DATA_WIDTH-1:0]	    sram_i,
+   		input  logic					            wr,
+   		input  logic					            rd, 
+        output logic signed [DATA_WIDTH-1:0]        sram_o
+  );
 
+  logic signed [DATA_WIDTH-1:0] mem [SRAM_SIZE-1:0];
 
-    always_ff @(posedge clk) begin
-            if(rst) begin
-                rd_pointer <= '0;  
-                wr_pointer <= '0;
-            end 
-            else if(sram_wr) begin
-                mem[wr_pointer] <= sram_i; 
-                if(wr_pointer == SRAM_SIZE-1) begin
-                    wr_pointer <= '0;
-                end 
-                else begin
-                    wr_pointer <= wr_pointer +1;
-                end
-            end 
-            else if(sram_rd) begin
-                sram_o <= mem[rd_pointer];
-                if(rd_pointer == SRAM_SIZE-1) begin
-                    rd_pointer <= '0;
-                end
-                else begin
-                    rd_pointer <= rd_pointer + 1;
-                end
-            end
+  always_ff @ (posedge clk) begin
+    if (rst) begin
+        sram_o <= '0;
     end
+    else if(wr) begin
+      mem[addr] <= sram_i;
+    end
+    else if(rd) begin 
+        sram_o <= mem[addr];
+    end
+  end
 
 endmodule
