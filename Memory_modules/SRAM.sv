6193a76598ad46e2cae872b1fecd3c6f53847c80
tgavrisa <tgavrisa@auth.gr>
2026-07-07 16:30:51 +0200

    Added handshake protocol to SRAM module

--- Memory_modules/SRAM.sv
+++ Memory_modules/SRAM.sv
@@ -4,23 +4,34 @@ module single_port_ram
   ( 	input  logic					                      clk, rst,
    		input  logic        [$clog2(SRAM_SIZE)-1:0]	addr,
    		input  logic signed [DATA_WIDTH-1:0]	      sram_i,
-   		input  logic					                      wr,
-   		input  logic					                      rd, 
-        output logic signed [DATA_WIDTH-1:0]      sram_o
+   		input  logic					                      wr, rd, 
+      output logic                                ram_valid, ram_ready,
+      output logic signed [DATA_WIDTH-1:0]        sram_o
   );
 
   logic signed [DATA_WIDTH-1:0] mem [SRAM_SIZE-1:0];
+  logic [$clog2(SRAM_SIZE)-1:0] counter = 0; 
+
+  assign ram_ready = (counter == SRAM_SIZE);
+
+  assign ram_valid = (counter < SRAM_SIZE);
 
   always_ff @ (posedge clk) begin
     if (rst) begin
         sram_o <= '0;
+        counter <= '0;
     end
-    else if(wr) begin
-      mem[addr] <= sram_i;
-    end
-    else if(rd) begin 
+    else begin
+      if(wr && ram_ready) begin
+        mem[addr] <= sram_i;  
+        counter <= counter + 1;
+      end
+      else if(rd && ram_valid) begin 
         sram_o <= mem[addr];
+        counter <= counter - 1;
+      end
     end
   end
+    
 
 endmodule
