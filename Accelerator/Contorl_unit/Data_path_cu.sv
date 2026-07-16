fbd43094f144d80db7f0bd22b23192bc1a829cec
tgavrisa <tgavrisa@auth.gr>
2026-07-16 14:23:37 +0200

    Fixed: communication between FSMs, state logic

--- Accelerator/Contorl_unit/Data_path_cu.sv
+++ Accelerator/Contorl_unit/Data_path_cu.sv
@@ -2,9 +2,11 @@ import pe_pkg::*;
 
 module control_unit (
     input  logic clk, rst, cu_hold,
-    input  logic stop_comp,                         // stop computing signal comes from the translator
-    input  logic start_w_load, start_comp,          // signals from mem_cu
-    input  logic flush_flag, dma_load_finish,       // accumulator flush TODO: add this to accumulator
+    input  logic stop_comp, start_comp,             // stop computing signal comes from the translator
+    input  logic start_w_load,                      // signals from mem_cu
+    input  logic sa_valid,
+
+    output logic results_ready,
     output logic sp_rd, sp_flush,                   // scratchpad signals
     output logic acc_op, acc_hold, acc_state,       // accumulator SRAM signals
     output logic sa_wr, sa_comp_e                   // systolic array signals
@@ -19,9 +21,11 @@ module control_unit (
     state_t next_state, curr_state;
     
     logic [$clog2(M)-1:0]          sp_count;
-    logic [$clog2(BATCH_SIZE)-1:0] comp_count; 
-    logic                          count_en, cnt_clr, comp_count_en, b_cnt_clr;
-    logic                          acc_state_flip = 0;
+    logic [$clog2(BATCH_SIZE)-1:0] comp_count, in_count, out_count;
+    logic                          in_cnt_clr, out_cnt_clr; 
+    logic                          count_en, cnt_clr, comp_count_en, b_cnt_clr, k_increase, in_count_en, k_clear;
+    logic                          acc_state_flip;
+    logic [$clog2(K_TILE)-1:0]     k_tile_count;
 
     always_ff @(posedge clk) begin
         if(rst || cu_hold) begin
@@ -62,16 +66,36 @@ module control_unit (
     end    
 
     always_ff @(posedge clk) begin
-        if(rst || b_cnt_clr) begin
+        if(rst) begin
             comp_count <= '0;
-        end    
-        else if(comp_count_en) begin
-            if (comp_count == (BATCH_SIZE-1)) begin
-                comp_count <= '0;
-            end 
-            else comp_count <= comp_count + 1;
+        end
+        else if(sa_valid && comp_count_en) begin
+            comp_count <= comp_count +1;
+        end
+    end 
+
+    always_ff @(posedge clk) begin
+        if(rst || k_clear) begin
+            k_tile_count <= '0;
+        end
+        else if(k_increase) begin
+            k_tile_count <= k_tile_count +1;
+        end
+    end 
+
+    always_ff @(posedge clk) begin
+        if(rst || in_cnt_clr) begin
+            in_count <= '0;
+        end
+        else if(in_count_en) begin
+            in_count <= in_count + 1;
         end 
-    end
+    end 
+    
+    always_ff @(posedge clk) begin
+        if(rst || out_cnt_clr) out_count <= '0;
+        else if(sa_valid)      out_count <= out_count + 1; 
+    end 
 
     always_comb begin
 
@@ -88,7 +112,18 @@ module control_unit (
         sa_wr = 0; 
         sa_comp_e = 0;
 
-        
+        results_ready = 0;
+        comp_count_en = 0;
+
+        count_en      = 0;
+        cnt_clr       = 0;
+        b_cnt_clr     = 0;
+
+        k_increase = 0;
+        k_clear = 0;
+
+        acc_op = (k_tile_count == 0) ? 1'b0 : 1'b1;
+
         case (curr_state)
             IDLE : begin 
                 if(start_w_load) begin
@@ -122,11 +157,28 @@ module control_unit (
 
                 comp_count_en = 1;
                 
-                if(comp_count == (BATCH_SIZE + M - 1)) begin
-                    acc_state_flip = 1;
-                    acc_op  = 1;
-                    b_cnt_clr = 1;
-                    next_state = COMP;
+                if (in_count < BATCH_SIZE) begin
+                    sp_rd       = 1'b1;
+                    in_count_en = 1'b1;
+                end
+                
+
+                if(sa_valid && (out_count == (BATCH_SIZE-1))) begin
+
+                    if(k_tile_count == (K_TILE-1)) begin
+                        acc_state_flip = 1;
+                        results_ready = 1;
+                        k_clear = 1;
+                    end 
+                    else begin
+                        
+                        k_increase = 1;
+                        next_state = COMP;
+                    end
+
+                    in_cnt_clr  = 1;
+                    out_cnt_clr = 1;
+                    b_cnt_clr = 1;   
                 end 
                 else if(stop_comp) begin
                     next_state = IDLE;
--- Accelerator/Contorl_unit/Memory_cu.sv
+++ Accelerator/Contorl_unit/Memory_cu.sv
@@ -1,10 +1,12 @@
 import pe_pkg::*;
 
-// TODO: add communication between the two FSMs
+
 module memory_unit (
     input  logic clk, rst, start_w, start_a,
     input  logic results_ready,                     // comes from the DP FSM
     input  logic flush_flag, dma_load_finish,       // accumulator flush TODO: add this to accumulator
+    
+    output logic start_w_load,
     output logic acc_rd,                            // accumulator SRAM signal
     output logic dma_go_pulse                       // dma signals
 );
@@ -15,11 +17,13 @@ module memory_unit (
         W_FETCH_WAIT = 3'd2,
         A_FETCH_REQ  = 3'd3,
         A_FETCH_WAIT = 3'd4, 
-        STORE        = 3'd5
+        STORE_REQ    = 3'd5,
+        STORE_WAIT   = 3'd6
     } state_t;
 
-    state_t                       curr_state, next_state;
-    logic                         count_max = M-1;
+    state_t                        curr_state, next_state;
+    logic                          cnt_clr;
+    logic [$clog2(BATCH_SIZE)-1:0] batch_count;
 
     always_ff @(posedge clk) begin
         if(rst) begin
@@ -36,6 +40,10 @@ module memory_unit (
         acc_rd    = 0;
 
         dma_go_pulse = 0;
+
+        start_w_load = 0;
+
+        cnt_clr = 0;     
                 
         case (curr_state)
             IDLE : begin // done
@@ -44,6 +52,9 @@ module memory_unit (
                 end
                 else if(start_a) begin
                     next_state = A_FETCH_REQ;
+                end
+                else if(results_ready) begin
+                    next_state = STORE_REQ;
                 end
                 else next_state = IDLE;
             end
@@ -56,12 +67,13 @@ module memory_unit (
             W_FETCH_WAIT : begin // done
                 
                 if (dma_load_finish) begin
+                    start_w_load = 1;
                     next_state = IDLE;
                 end
                 else begin
                     next_state = W_FETCH_WAIT;
                 end
-            end
+            end 
 
             A_FETCH_REQ : begin // done
                 dma_go_pulse = 1; // 1 clock cycle
@@ -76,14 +88,21 @@ module memory_unit (
                 else next_state = A_FETCH_WAIT;
             end
 
-            STORE : begin
-                acc_rd = 1;
+            STORE_REQ : begin
+                dma_go_pulse = 1'b1; 
+                next_state   = STORE_WAIT;
+            end
 
-                if(results_ready) begin
+            STORE_WAIT : begin
+                acc_rd = 1'b1; 
+
+                if(dma_load_finish) begin
                     next_state = IDLE;
                 end
-                else next_state = STORE;
-            end
+                else begin 
+                    next_state = STORE_WAIT;
+                end
+            end 
             
             default: begin
                 next_state = IDLE;
