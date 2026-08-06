7ac7191d9c4c64cef8f1ad5a5f9e1376005ae8eb
tgavrisa <tgavrisa@auth.gr>
2026-08-06 15:43:02 +0200

    Fixed: naming conventions into more readable forms

--- Accelerator/RoCCTran.sv
+++ Accelerator/RoCCTran.sv
@@ -19,7 +19,7 @@ module RoCCTran (
     input  logic [63:0] core_cmd_rs2,
 
     // Busy signal
-    input  logic        busy,
+    input  logic        dma_busy_i,
 
     output logic        core_cmd_ready_o,
 
@@ -29,15 +29,15 @@ module RoCCTran (
     output logic [31:0] dma_dst_addr_o,
 
     // DP FSM signals
-    output logic cu_hold, 
-    output logic stop_comp,
+    output logic cu_hold_o, 
+    output logic stop_comp_o,
 
     // Mem FSM signals 
-    output logic start_w, 
-    output logic start_a                   
+    output logic start_w_o, 
+    output logic start_a_o                   
 );  
 
-    assign core_cmd_ready_o = !busy;
+    assign core_cmd_ready_o = !dma_busy_i;
 
     always_ff @(posedge clk) begin 
         if (rst) begin
@@ -48,42 +48,42 @@ module RoCCTran (
             dma_dst_addr_o <= 0;
 
             // DP FSM signals
-            cu_hold    <= 0; 
-            stop_comp  <= 0; 
+            cu_hold_o    <= 0; 
+            stop_comp_o  <= 0; 
 
             // Mem FSM signals 
-            start_w <= 0; 
-            start_a <= 0;
+            start_w_o <= 0; 
+            start_a_o <= 0;
         end 
         else begin
 
-            start_w   <= 0; 
-            start_a   <= 0;
-            stop_comp <= 0;
+            start_w_o   <= 0; 
+            start_a_o   <= 0;
+            stop_comp_o <= 0;
 
             if (core_cmd_valid && core_cmd_ready_o) begin
 
                 case (core_cmd_funct)
                     7'd1 : begin // W_FETCH
-                        start_w <= 1;
+                        start_w_o <= 1;
                         dma_src_addr_o <= core_cmd_rs1;
                         dma_length_o   <= core_cmd_rs2[63:32];
                         dma_dst_addr_o <= core_cmd_rs2[31:0];
                     end
 
                     7'd2 : begin // A_FETCH
-                        start_a <= 1;
+                        start_a_o <= 1;
                         dma_src_addr_o <= core_cmd_rs1;
                         dma_length_o   <= core_cmd_rs2[63:32];
                         dma_dst_addr_o <= core_cmd_rs2[31:0];
                     end
 
                     7'd4 : begin // HOLD
-                        cu_hold    <= 1;
+                        cu_hold_o    <= 1;
                     end
 
                     7'd16 : begin // STOP COMP
-                        stop_comp  <= 1;
+                        stop_comp_o  <= 1;
                     end
 
                     default: begin
