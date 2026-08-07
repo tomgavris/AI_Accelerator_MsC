a9709c258b122d97e63ac2208b25a615dc550532
tgavrisa <tgavrisa@auth.gr>
2026-08-07 12:02:41 +0200

    Fixed: width mismatches in if statement. Added: CHUNKS parameter to make the module robust

--- Memory_modules/Concat.sv
+++ Memory_modules/Concat.sv
@@ -6,24 +6,29 @@ module concat (
     output logic                         valid_o,
     output logic signed [SRAM_WIDTH-1:0] concat_o
 );
-    logic [$clog2(CONC_ADD)-1:0]   counter;
-    logic [DATA_WIDTH-1:0]         raw_data [CONC_ADD-1:0];
+    // Chunks paremeter shields the module from running it problems with other parameters
+    localparam CHUNKS = SRAM_WIDTH / 64;
+
+    logic [$clog2(CHUNKS+1)-1:0]   counter;
+    logic [CHUNKS-1:0][63:0]       raw_data;
 
     always_ff @(posedge clk) begin
         if (rst) begin
             counter  <= '0;
             concat_o <= '0;
             valid_o  <= 1'b0;
-            for (int i = 0; i < CONC_ADD; i++) raw_data[i] <= '0;
+            for (int i = 0; i < CHUNKS; i++) raw_data[i] <= '0;
         end else begin
             valid_o <= 1'b0;
             if (enable) begin
                 raw_data[counter] <= concat_i;
 
-                if (counter == CONC_ADD-1) begin
-                    for (int i = 0; i < CONC_ADD-1; i++)
-                        concat_o[i*DATA_WIDTH +: DATA_WIDTH] <= raw_data[i];
-                    concat_o[(CONC_ADD-1)*DATA_WIDTH +: DATA_WIDTH] <= concat_i; 
+                if (counter == CHUNKS-1) begin
+                    for (int i = 0; i < CHUNKS-1; i++)
+                        concat_o[i*64 +: 64] <= raw_data[i];
+
+                    // The last data package should not be delayed by the raw_data register
+                    concat_o[(CHUNKS-1)*64 +: 64] <= concat_i; 
                     valid_o <= 1'b1;
                     counter <= '0;
                 end else begin
