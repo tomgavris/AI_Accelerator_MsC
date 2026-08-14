52f67d189f0fa66231166c32c41fd728d7ebdbf5
tgavrisa <tgavrisa@auth.gr>
2026-08-14 15:52:37 +0200

    Divided SP to Activations and Weights SP

---
+++ Memory_modules/Activations_sp.sv
@@ -0,0 +1,41 @@
+import pe_pkg::*;
+
+module activations_sp (
+    input  logic signed [PARTITIONS-1:0][CONC_ADD-1:0][N-1:0][OP-1:0][DATA_WIDTH-1:0]        sp_i,
+    input  logic                                                                             clk, rst,
+    input  logic        [PARTITIONS-1:0]                                                     sp_rd_i, 
+    input  logic        [PARTITIONS-1:0]                                                     sp_wr_i, 
+    input  logic                                                                             sp_state_i,  
+    input  logic        [PARTITIONS-1:0][$clog2(SRAM_SIZE)-1:0]                              sp_wr_add, sp_rd_add, 
+    output logic                                                                             sp_ready_o, sp_valid_o,
+    output logic signed [PARTITIONS-1:0][CONC_ADD-1:0][N-1:0][OP-1:0][DATA_WIDTH-1:0]        sp_o
+);
+
+    logic [PARTITIONS-1:0] valid_wire, ready_wire, rd_wire, state_wire;
+
+    assign sp_ready_o   = 1'b1; 
+    assign sp_valid_o   = 1'b1; 
+
+    assign rd_wire    = sp_rd_i;
+    assign state_wire = {PARTITIONS{sp_state_i}};
+    
+    genvar i;
+    generate
+        for (i = 0; i < PARTITIONS; i++) begin : db_banks
+            double_buffer #(.DB_WIDTH(CONC_ADD*N*OP*DATA_WIDTH)) DB_inst (
+                .db_i(sp_i[i]),
+                .clk(clk),
+                .rst(rst),
+                .state(state_wire[i]),
+                .db_wr(sp_wr_i[i]), 
+                .db_rd(sp_rd_i[i]),
+                .db_ready(ready_wire[i]), 
+                .db_valid(valid_wire[i]),
+                .db_wr_add(sp_wr_add[i]),
+                .db_rd_add(sp_rd_add[i]),
+                .db_o(sp_o[i])
+            );
+        end
+    endgenerate
+
+endmodule
---
+++ Memory_modules/Weights_sp.sv
@@ -0,0 +1,31 @@
+import pe_pkg::*;
+
+module weights_sp (
+    input  logic signed [M-1:0][N-1:0][N-1:0][OP-1:0][DATA_WIDTH-1:0]        sp_i,
+    input  logic                                                             clk, rst,
+    input  logic                                                             sp_rd_i, 
+    input  logic                                                             sp_wr_i, 
+    input  logic                                                             sp_state_i,  
+    input  logic        [$clog2(WEIGHT_SP_SIZE)-1:0]                         sp_wr_add, sp_rd_add, 
+    output logic                                                             sp_ready_o, sp_valid_o,
+    output logic signed [M-1:0][N-1:0][N-1:0][OP-1:0][DATA_WIDTH-1:0]        sp_o
+);
+
+    localparam WEIGHT_SP_WIDTH = M*N*N*OP*DATA_WIDTH;
+    
+    double_buffer #(.DB_WIDTH(WEIGHT_SP_WIDTH), .DB_SIZE(WEIGHT_SP_SIZE)) DB_inst (
+        .db_i(sp_i),
+        .clk(clk),
+        .rst(rst),
+        .state(sp_state_i),
+        .db_wr(sp_wr_i), 
+        .db_rd(sp_rd_i),
+        .db_ready(sp_ready_o), 
+        .db_valid(sp_valid_o),
+        .db_wr_add(sp_wr_add),
+        .db_rd_add(sp_rd_add),
+         .db_o(sp_o)
+    );
+            
+
+endmodule
