1c905e333b8c74a111d3cd53cd1abe93ec5a88b1
tgavrisa <tgavrisa@auth.gr>
2026-08-27 14:50:02 +0300

    Changed and cleaned up Memory size and Batch parameters

--- pe_pkg.sv
+++ pe_pkg.sv
@@ -1,32 +1,34 @@
 package pe_pkg;
 
-    // parameters relating to data width
+    // Data width parameters
     localparam DATA_WIDTH = 8;
     localparam P_DATA_WIDTH = 16;
 
-    // parameters relating to the SA architecture
+    // SA parameters  
     localparam OP = 2;
-    localparam N = 2;
-    localparam M = 2;
+    localparam N = 4;
+    localparam M = 8;
 
 
     localparam CYCLES = 2;
     localparam W_CYCLES = M;
 
-    // parameters relating to memory
+    // Memory parameters
     localparam PARTITIONS = 4;
-    localparam SP_SIZE = 2048; // Scratchpad size
-    localparam ACC_SIZE = 512;
-    localparam SRAM_SIZE = SP_SIZE/PARTITIONS;
     localparam CONC_ADD = M/PARTITIONS; // Concurrent addresses read from each partition
     localparam CONC_ADD_D = CONC_ADD*N*OP; // The sum of the data read form an address
-    localparam SRAM_WIDTH = CONC_ADD*N*OP*DATA_WIDTH; // One address read for each partition  
-
-    // DMA parameters
-    localparam WEIGHT_LEN = 256;
-    localparam SP_ADDR    = 32'd0;
+    localparam SRAM_WIDTH = CONC_ADD*N*OP*DATA_WIDTH; // One address read for each partition
 
     // Batch parameters
-    localparam BATCH_SIZE = 64;
-    localparam K_TILE = 64;         // K is the number of tiles needed before we get one result
+    localparam BATCH_SIZE = 8;
+    localparam K_TILE = 8;         // K is the number of tiles needed before we get one result
+
+    // Memory sizes
+    localparam SP_SIZE = 2048; // Scratchpad size
+    localparam SRAM_SIZE = SP_SIZE/PARTITIONS;  
+    localparam WEIGHT_SP_SIZE  = M; 
+    localparam ACC_SIZE = BATCH_SIZE;
+
+    // DMA parameters
+    localparam SP_ADDR    = 32'd0;
 endpackage
