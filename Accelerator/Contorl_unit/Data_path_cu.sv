fadf9365619c5b8a3726c531e10f164a95945e8a
tgavrisa <tgavrisa@auth.gr>
2026-08-30 17:56:14 +0300

    Added reading_done signal

--- Accelerator/Contorl_unit/Data_path_cu.sv
+++ Accelerator/Contorl_unit/Data_path_cu.sv
@@ -1,16 +1,16 @@
 import pe_pkg::*;
 
 module dp_fsm (
-    input  logic                            clk, rst, 
-    input  logic                            stop_comp_i, cu_hold_i,            // RoCC Tran. signals
-    input  logic                            start_w_load_i, start_comp_i,      // Mem FSM signal
-    input  logic                            sa_valid_i, 
+    input  logic                               clk, rst, 
+    input  logic                               stop_comp_i, cu_hold_i,            // RoCC Tran. signals
+    input  logic                               start_w_load_i, start_comp_i,      // Mem FSM signal
+    input  logic                               sa_valid_i, 
 
-    output logic                            results_ready_o, no_skew, dp_busy_o,
-    output logic                            w_sp_rd_o, a_sp_rd_o,              // SP signals
-    output logic                            acc_op_o, acc_hold_o, acc_state_o, // Accumulator signals
-    output logic                            sa_wr_o, sa_comp_e_o,              // SA signals
-    output logic [$clog2(BATCH_SIZE)-1:0]   out_count                          // Accumulator addressing
+    output logic                               results_ready_o, no_skew, dp_busy_o, 
+    output logic                               w_sp_rd_o, a_sp_rd_o,              // SP signals
+    output logic                               acc_op_o, acc_hold_o, acc_state_o, // Accumulator signals
+    output logic                               sa_wr_o, sa_comp_e_o,              // SA signals
+    output logic [$clog2(BATCH_SIZE)-1:0]      out_count                          // Accumulator addressing
 );
 
     typedef enum logic [1:0] { 
@@ -28,7 +28,13 @@ module dp_fsm (
     logic                          acc_state_flip;
     logic [$clog2(K_TILE)-1:0]     k_tile_count;
     logic                          w_ready_flag, reg_clr;
-    logic                          in_cnt_clr_o;            
+    logic                          in_cnt_clr_o; 
+    logic                          reading_done;      
+
+    always_ff @(posedge clk) begin
+        if (rst || in_cnt_clr_o) reading_done <= 1'b0;
+        else if (in_count == BATCH_SIZE-1) reading_done <= 1'b1;
+    end    
 
     always_ff @(posedge clk) begin
         if(rst || cu_hold_i) begin
@@ -77,7 +83,7 @@ module dp_fsm (
             k_tile_count <= '0;
         end
         else if(k_increase) begin
-            k_tile_count <= k_tile_count +1;
+            k_tile_count <= k_tile_count + 1;
         end
     end 
 
@@ -110,29 +116,20 @@ module dp_fsm (
         
         in_cnt_clr_o = 0;
         out_cnt_clr  = 0;
+        in_count_en  = 0;
 
-        // setting all signals to 0
         no_skew = 0; 
-
         w_sp_rd_o = 0;
         a_sp_rd_o = 0;
-
         acc_op_o = 0; 
-        
-
         acc_state_flip = 0;
-
         sa_wr_o = 0; 
         sa_comp_e_o = 0;
-
         results_ready_o = 0;
-
         count_en      = 0;
         cnt_clr       = 0;
-
         k_increase    = 0;
         k_clear       = 0;
-
         reg_clr       = 0;
 
         acc_op_o = (k_tile_count == 0) ? 1'b0 : 1'b1;
@@ -140,7 +137,7 @@ module dp_fsm (
         case (curr_state)
             IDLE : begin 
                 if(w_ready_flag) begin
-                    reg_clr   = 1;
+                    reg_clr    = 1;
                     next_state = W_LOAD;
                 end
                 else if (start_comp_i) begin
@@ -149,48 +146,41 @@ module dp_fsm (
                 else next_state = IDLE;
             end
 
+            W_LOAD : begin 
+                no_skew     = 1;
+                w_sp_rd_o   = 1; 
+                sa_wr_o     = 1;
+                count_en    = 1;
 
-            W_LOAD : begin // done
-
-                        no_skew     = 1;
-
-                        w_sp_rd_o   = 1; 
-                        
-                        sa_wr_o     = 1;
-
-                        count_en  = 1;
-
-                        if(sp_count == (M-1)) begin
-                            next_state = IDLE;
-                            cnt_clr  = 1;
-                        end 
-                        else begin
-                            next_state = W_LOAD;
-                        end
-                    end
+                if(sp_count == (M-1)) begin
+                    next_state = IDLE;
+                    cnt_clr  = 1;
+                end 
+                else begin
+                    next_state = W_LOAD;
+                end
+            end
             
             COMP : begin
-                sa_comp_e_o     = 1;
+                // Keep compute enable high during normal run and drain phase
+                sa_comp_e_o = 1;
 
-                if (in_count < BATCH_SIZE) begin
+                if (!reading_done) begin
                     a_sp_rd_o   = 1'b1;
                     in_count_en = 1'b1;
                 end          
 
-
+                // Allow out_count to finish completely before shutting down
                 if(sa_valid_i && (out_count == (BATCH_SIZE-1))) begin
                     acc_state_flip = 1;
-                    sa_comp_e_o    = 0;
                     if(k_tile_count == (K_TILE-1)) begin
-                        
                         results_ready_o = 1;
                         k_clear = 1;
-                        next_state = IDLE;
                     end 
                     else begin
                         k_increase = 1;
                     end
-                    next_state = IDLE;
+                    next_state  = IDLE;
                     in_cnt_clr_o  = 1;
                     out_cnt_clr   = 1;
                 end 
@@ -203,7 +193,4 @@ module dp_fsm (
             end
         endcase
     end
-        
-
-
 endmodule
