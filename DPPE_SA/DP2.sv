fe07ab01efea23e5465b06de74c1908d6ad875af
tgavrisa <tgavrisa@auth.gr>
2026-08-31 16:47:13 +0300

    Changed to tree adder for timing optimisation

--- DPPE_SA/DP2.sv
+++ DPPE_SA/DP2.sv
@@ -1,28 +1,42 @@
 import pe_pkg::*;
 
-module DP(
+module DP (
     input  logic signed [OP-1:0][DATA_WIDTH-1:0]  a_i,
-    input  logic signed [OP-1:0][DATA_WIDTH-1:0]  w_i, 
-    input  logic signed [P_DATA_WIDTH-1:0]        parsum_i, //parsum_i = partial sum output
-    input  logic                                  comp_e, //comp_e = compute enable
-    output logic signed [OP-1:0][DATA_WIDTH-1:0]  a_o, 
-    output logic signed [P_DATA_WIDTH-1:0]        parsum_o //a_o = activation output, parsum_o = partial sum output 
+    input  logic signed [OP-1:0][DATA_WIDTH-1:0]  w_i,
+    input  logic signed [P_DATA_WIDTH-1:0]        parsum_i,
+    input  logic                                  comp_e,
+    output logic signed [OP-1:0][DATA_WIDTH-1:0]  a_o,
+    output logic signed [P_DATA_WIDTH-1:0]        parsum_o
 );
 
-logic signed [P_DATA_WIDTH-1:0] temp;
+    localparam int POW2_OP = 1 << $clog2(OP);
+    localparam int LEVELS  = $clog2(POW2_OP);
 
-always_comb begin 
-    if (comp_e == 1) begin
-        temp = parsum_i;
-        for (int i = 0; i < OP ; i++) begin
-            a_o[i] = a_i[i];
-            temp   = temp + a_i[i]*w_i[i];
-        end 
-        parsum_o = temp;
-    end else begin
-        a_o      = '0;
+    logic signed [P_DATA_WIDTH-1:0] tree [LEVELS+1][POW2_OP];
+
+    always_comb begin
+        a_o = '0;
         parsum_o = '0;
-        temp     = '0;
+        for (int lvl = 0; lvl <= LEVELS; lvl++) begin
+            for (int i = 0; i < POW2_OP; i++) begin
+                tree[lvl][i] = '0;
+            end
+        end
+
+        if (comp_e) begin
+            a_o = a_i;
+            for (int i = 0; i < POW2_OP; i++) begin
+                if (i < OP) begin
+                    tree[0][i] = a_i[i] * w_i[i];
+                end
+            end
+            for (int lvl = 1; lvl <= LEVELS; lvl++) begin
+                for (int i = 0; i < (POW2_OP >> lvl); i++) begin
+                    tree[lvl][i] = tree[lvl-1][i*2] + tree[lvl-1][i*2 + 1];
+                end
+            end
+            parsum_o = parsum_i + tree[LEVELS][0];
+        end
     end
-end
+
 endmodule
